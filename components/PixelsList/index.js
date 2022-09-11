933633a275e89b051c2562b5701646903679ced6
Enzo3322 <enzo.spag14@gmail.com>
2022-09-11 18:51:14 -0300

    intial

---
+++ components/PixelsList/index.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import styles from './styles.module.scss';
+
+export const PixelsList = ({ pixelArr }) => {
+	const [search, setSearch] = useState('');
+	const [filteredPixels, setFilteredPixels] = useState([]);
+
+	useEffect(() => {
+		if (pixelArr) {
+			const filter = pixelArr?.filter((pixel) =>
+				pixel.map.toLowerCase().includes(search.toLowerCase())
+			);
+			setFilteredPixels(filter);
+		} else {
+			setFilteredPixels(pixelArr);
+		}
+	}, [search]);
+
+	if (!pixelArr) return null;
+
+	return (
+		<>
+			<div className={styles.searchContainer}>
+				<input
+					type="text"
+					placeholder="Buscar mapa..."
+					className={styles.search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</div>
+			<section className={styles.grid}>
+				{filteredPixels?.map((item, i) => (
+					<a key={i} href={item.pid}>
+						<picture>
+							<img src={item?.thumb?.url} alt={item?.thumb?.alt} />
+						</picture>
+						<p className={styles.name}>{item.map}</p>
+					</a>
+				))}
+				{filteredPixels.length === 0 && <h2>Nenhum mapa encontrado</h2>}
+			</section>
+		</>
+	);
+};
---
+++ mock/mock.js
@@ -0,0 +1,158 @@
+const agents = [
+	'Brimstone',
+	'Reyna',
+	'Neon',
+	'KillJoy',
+	'Omen',
+	'Breach',
+	'Jett',
+	'Astra',
+	'Skye',
+	'Kayo',
+	'Chamber',
+	'Sova',
+	'Raze',
+	'Phoenix',
+	'Yoru',
+	'Viper',
+	'Fade',
+	'Cypher',
+	'Sage',
+];
+export const pixels = [
+	{
+		pid: 1,
+		thumb: {
+			url: './maps/breeze.png',
+			alt: 'Breeze img',
+		},
+		map: 'Breeze',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 2,
+		thumb: {
+			url: './maps/ascent.png',
+			alt: '',
+		},
+		map: 'Ascent',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 3,
+		thumb: {
+			url: './maps/bind.png',
+			alt: '',
+		},
+		map: 'Bind',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 4,
+		thumb: {
+			url: './maps/icebox.png',
+			alt: '',
+		},
+		map: 'Icebox',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 5,
+		thumb: {
+			url: './maps/fracture.png',
+			alt: '',
+		},
+		map: 'Fracture',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 6,
+		thumb: {
+			url: './maps/pearl.png',
+			alt: '',
+		},
+		map: 'Pearl',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 7,
+		thumb: {
+			url: './maps/haven.png',
+			alt: '',
+		},
+		map: 'Haven',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+	{
+		pid: 8,
+		thumb: {
+			url: './maps/split.png',
+			alt: 'Breeze img',
+		},
+		map: 'Split',
+		img: [
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+			{ url: '', alt: '' },
+		],
+		agent: agents,
+	},
+];
+
+export const agentsPixels = [
+	{
+		agent: 'Viper',
+		agent_img: '',
+		pixels: [
+			{
+				map: 'Breeze',
+				local: '',
+				description: [
+					'Aliquip duis mollit ullamco dolore ea cillum est commodo. Laboris ex aute laborum veniam sit nisi labore consectetur proident deserunt esse veniam. Do excepteur irure sit eiusmod laborum.',
+					'Aliquip duis mollit ullamco dolore ea cillum est commodo. Laboris ex aute laborum veniam sit nisi labore consectetur proident deserunt esse veniam. Do excepteur irure sit eiusmod laborum.',
+					'Aliquip duis mollit ullamco dolore ea cillum est commodo. Laboris ex aute laborum veniam sit nisi labore consectetur proident deserunt esse veniam. Do excepteur irure sit eiusmod laborum.',
+				],
+				imgs: [
+					{ url: '', alt: '' },
+					{ url: '', alt: '' },
+					{ url: '', alt: '' },
+				],
+			},
+		],
+	},
+];
