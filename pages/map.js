b6c29f75c9cbad54cccb5b50ee11540181e37771
Enzo3322 <enzo.spag14@gmail.com>
2022-09-11 21:47:41 -0300

    creating routes for agents

--- pages/map.js
+++ pages/map.js
@@ -1,22 +1,58 @@
-import { useRouter } from "next/router";
-import { pixels } from "../mock/mock";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { pixels } from '../mock/mock';
+import styles from '../styles/map.module.scss';
 
 export default function Map() {
-  const router = useRouter()
-  const { ref } = router.query
-  const mapInfos = pixels.find((mapa) => mapa.name === ref)
+	const router = useRouter();
+	const { ref } = router.query;
+	const mapInfos = pixels.find((mapa) => mapa.name === ref);
 
-  if (!mapInfos) return <p>Opss... Parece que algo deu errado! <br /> Que tal voltar para onde estava? <a href="/">Home</a></p>
-  return (
-    <>
-      {mapInfos.agents.map((agent, i) => (
-        <div key={i}>
-          <a>
-            <img src={agent.agent_img} />
-            <p>{agent.name}</p>
-          </a>
-        </div>
-      ))}
-    </>
-  )
+	const [search, setSearch] = useState('');
+	const [filteredAgents, setFilteredAgents] = useState(mapInfos?.agents);
+
+	useEffect(() => {
+		const filter = mapInfos?.agents?.filter((agent) =>
+			agent?.name?.toLowerCase().includes(search.toLowerCase())
+		);
+		setFilteredAgents(filter);
+	}, [search, mapInfos]);
+
+	if (!mapInfos) {
+		return (
+			<p>
+				Opss... Parece que algo deu errado! <br /> Que tal voltar para onde
+				estava? <Link href="/">Home</Link>
+			</p>
+		);
+	}
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.thumbContainer}>
+				<h1>{mapInfos.name}</h1>
+				<picture>
+					<img src={mapInfos.thumb.url} alt={mapInfos.thumb.alt} />
+				</picture>
+			</div>
+			<div className={styles.searchContainer}>
+				<input
+					type="text"
+					placeholder="Pesquise pelo nome do seu agente..."
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</div>
+			<div className={styles.agentContainer}>
+				{filteredAgents?.map((agent, i) => (
+					<a key={i} href={`/pixel?map=${mapInfos.name}&agent=${agent.name}`}>
+						<picture>
+							<img src={agent.agent_img} alt="" />
+						</picture>
+						<p>{agent.name}</p>
+					</a>
+				))}
+			</div>
+		</div>
+	);
 }
--- pages/pixel.js
+++ pages/pixel.js
@@ -1,9 +1,30 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
+import { pixels } from '../mock/mock';
 
 export default function Pixel() {
-  const router = useRouter()
-  const { pid } = router.query
+	const router = useRouter();
+	if (!pixels) return null;
 
+	const { map, agent } = router.query;
 
+	const matchMap = pixels?.find(
+		(pixel) => pixel?.name.toLowerCase() === map?.toLowerCase()
+	);
 
+	const matchAgent = matchMap?.agents.find(
+		(agentName) => agentName.name === agent
+	);
+
+	if (!matchAgent) return <p>Ops temos um problema</p>;
+
+	return (
+		<div>
+			<p>{matchAgent.name}</p>
+			<ul>
+				{matchAgent.pixels?.map((pixel, i) => (
+					<p key={i}>{pixel.description}</p>
+				))}
+			</ul>
+		</div>
+	);
 }
